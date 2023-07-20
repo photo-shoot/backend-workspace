17536255b1e7296e7b88a48400585441ff313b5c
bakhuiwon326 <powerup326@naver.com>
2023-07-21 02:28:24 +0900

    set spring-boot project

---
+++ src/main/java/photoshoot/backendworkspace/BackendWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package photoshoot.backendworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWorkspaceApplication.class, args);
+	}
+
+}
